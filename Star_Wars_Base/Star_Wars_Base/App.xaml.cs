1cc1c99e03dc9d886ebbb343f9af86bb8236aed7
Jean C. Mendoza <73866099+MenJCP@users.noreply.github.com>
2021-04-10 18:49:19 -0400

    Initial commit

---
+++ Star_Wars_Base/Star_Wars_Base/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Star_Wars_Base
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null): base(initializer) { }
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+
+
+
+
+    }
+}
---
+++ Star_Wars_Base/Star_Wars_Base/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Star_Wars_Base
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
