3cabaf19e3afb49f8ed02aec1952821d4ce4c553
elgaryy <manuel.adames88@gmail.com>
2021-04-11 00:48:11 -0400

    HomeViewModel Created

---
+++ Star_Wars_Base/Star_Wars_Base/Models/MenuOption.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Star_Wars_Base.Models
+{
+    public class MenuOption
+    {
+        public string Title { get; set; }
+        public string Image { get; set; }
+    }
+}
--- Star_Wars_Base/Star_Wars_Base/ViewModels/HomeViewModel.cs
+++ Star_Wars_Base/Star_Wars_Base/ViewModels/HomeViewModel.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Star_Wars_Base.Models;
 
 namespace Star_Wars_Base.ViewModels
 {
     public class HomeViewModel
     {
+        public MenuOption Films { get; set; } = new MenuOption();
+        public MenuOption People { get; set; } = new MenuOption();
+        public MenuOption Planets { get; set; } = new MenuOption();
+        public MenuOption Species { get; set; } = new MenuOption();
+        public MenuOption Starships { get; set; } = new MenuOption();
+        public MenuOption Vehicles { get; set; } = new MenuOption();
+
+        public HomeViewModel()
+        {
+            Films.Title = "Films"; //Films.Image = ".png";
+            People.Title = "People"; //People.Image = ".png";
+            Planets.Title = "Planets"; //Planets.Image = ".png";
+            Species.Title = "Species"; //Species.Image = ".png";
+            Starships.Title = "Starships"; //Starships.Image = ".png";
+            Vehicles.Title = "Vehicles"; //Vehicles.Image = ".png";
+        }
 
     }
 }
